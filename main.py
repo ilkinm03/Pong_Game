8f3eebbde7352b3d313b981e16b92be404b42c6f
ilkinm03 <ilkinm2003@gmail.com>
2023-03-05 11:13:49 +0400

    create first paddle raw

--- main.py
+++ main.py
@@ -1,8 +1,34 @@
-from turtle import Screen
+from turtle import Screen, Turtle
 
 screen = Screen()
 screen.setup(800, 600)
 screen.bgcolor("black")
 screen.title("PONG")
+screen.tracer(0)
+
+paddle = Turtle("square")
+paddle.color("white")
+paddle.penup()
+paddle.shapesize(5, 1)
+paddle.setpos(350, 0)
+
+
+def go_up():
+    new_y = paddle.ycor() + 20
+    paddle.goto(paddle.xcor(), new_y)
+
+
+def go_down():
+    new_y = paddle.ycor() - 20
+    paddle.goto(paddle.xcor(), new_y)
+
+
+screen.listen()
+screen.onkey(go_up, "Up")
+screen.onkey(go_down, "Down")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
 
 screen.exitonclick()
