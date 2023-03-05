7ba5c57517c57cbcd24f3a0720858b792289bb64
ilkinm03 <ilkinm2003@gmail.com>
2023-03-05 13:04:41 +0400

    create scoreboard

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import time
 from turtle import Screen
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 
 WIDTH = 800
 HEIGHT = 600
@@ -14,7 +15,7 @@ screen.tracer(0)
 
 r_paddle = Paddle((WIDTH // 2 - 50, 0))
 l_paddle = Paddle((-WIDTH // 2 + 50, 0))
-
+scoreboard = Scoreboard()
 ball = Ball()
 
 screen.listen()
@@ -26,15 +27,17 @@ screen.onkey(l_paddle.go_down, "s")
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
-    ball.move()
+    ball.move() 
     if ball.ycor() > HEIGHT / 2 - ball.width() or ball.ycor() < -HEIGHT / 2 - ball.width():
         ball.bounce_y()
     if (ball.distance(r_paddle) < 50 and ball.xcor() > WIDTH / 2 - 80) \
             or (ball.distance(l_paddle) < 50 and ball.xcor() < -WIDTH / 2 + 80):
         ball.bounce_x()
     if ball.xcor() > WIDTH / 2 + 20:
+        scoreboard.l_point()
         ball.reset()
     if ball.xcor() < -WIDTH / 2 - 20:
+        scoreboard.r_point()
         ball.reset()
     screen.update()
 
