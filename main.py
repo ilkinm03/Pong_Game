db8a7e7db6980d87e81b6f940317d5ad168ab4e0
ilkinm03 <ilkinm2003@gmail.com>
2023-03-05 12:44:16 +0400

    detect ball collision with paddle

--- main.py
+++ main.py
@@ -3,14 +3,17 @@ from turtle import Screen
 from paddle import Paddle
 from ball import Ball
 
+WIDTH = 800
+HEIGHT = 600
+
 screen = Screen()
-screen.setup(800, 600)
+screen.setup(WIDTH, HEIGHT)
 screen.bgcolor("black")
 screen.title("PONG")
 screen.tracer(0)
 
-r_paddle = Paddle((350, 0))
-l_paddle = Paddle((-350, 0))
+r_paddle = Paddle((WIDTH // 2 - 50, 0))
+l_paddle = Paddle((-WIDTH // 2 + 50, 0))
 
 ball = Ball()
 
@@ -22,8 +25,13 @@ screen.onkey(l_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
-    time.sleep(0.02)
+    time.sleep(0.1)
     ball.move()
+    if ball.ycor() > HEIGHT / 2 - ball.width() or ball.ycor() < -HEIGHT / 2 - ball.width():
+        ball.bounce_y()
+    if (ball.distance(r_paddle) < 50 and ball.xcor() > WIDTH / 2 - 80) \
+            or (ball.distance(l_paddle) < 50 and ball.xcor() < -WIDTH / 2 + 80):
+        ball.bounce_x()
     screen.update()
 
 screen.exitonclick()
